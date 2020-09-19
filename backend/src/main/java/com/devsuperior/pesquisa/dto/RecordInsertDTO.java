07d79edda4718a670c61fb401cb0b33cb1748aea
RobertoCarnauba <betocarnauba@hotmail.com>
2020-09-19 10:13:43 -0300

    Post [records]

---
+++ backend/src/main/java/com/devsuperior/pesquisa/dto/RecordInsertDTO.java
@@ -0,0 +1,65 @@
+package com.devsuperior.pesquisa.dto;
+
+import java.io.Serializable;
+
+public class RecordInsertDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private Integer age;
+	private Long gameId;
+	
+	public RecordInsertDTO() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Long getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(Long gameId) {
+		this.gameId = gameId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((gameId == null) ? 0 : gameId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecordInsertDTO other = (RecordInsertDTO) obj;
+		if (gameId == null) {
+			if (other.gameId != null)
+				return false;
+		} else if (!gameId.equals(other.gameId))
+			return false;
+		return true;
+	}
+	
+	
+}
