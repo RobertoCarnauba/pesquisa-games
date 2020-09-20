857a47fe26e0fccce570a905d8654b2da9ec77f5
RobertoCarnauba <betocarnauba@hotmail.com>
2020-09-20 18:28:16 -0300

    Config front-web to Netlify

--- front-web/src/components/pages/Records/index.tsx
+++ front-web/src/components/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import {formatDate} from './helpers'
 import Filters from '../../Filters'
 import  Pagination  from './Pagination'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = ' https://sds-pesqui.herokuapp.com';
 
 const Records = () => {
   const [recordsResponse, setRecordResponse] =  useState<RecordsResponse>();
